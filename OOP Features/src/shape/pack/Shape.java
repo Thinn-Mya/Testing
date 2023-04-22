861daade58f4babeb98cfd50b602c35b8b515019
IMU <IMU@DESKTOP-3BPEO1L>
2023-04-22 13:50:53 +0630

    commit2

---
+++ OOP Features/src/shape/pack/Shape.java
@@ -0,0 +1,28 @@
+package shape.pack;
+
+public class Shape {
+private String color;
+
+public Shape(String color) {
+	this.color = color;
+}
+
+public void area()
+{
+	System.out.println("Finding Area: ");
+}
+
+public String getColor() {
+	return color;
+}
+
+public void setColor(String color) {
+	this.color = color;
+}
+
+
+
+
+
+
+}
